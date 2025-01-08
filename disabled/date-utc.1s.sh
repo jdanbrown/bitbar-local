26b530ab023d2e338c6f077bba831b4d49da1421
Dan Brown <jdanbrown@gmail.com>
2025-01-08 16:15:26 -0700

    Disable date-utc.1s.sh

--- disabled/date-utc.1s.sh
+++ disabled/date-utc.1s.sh
@@ -2,7 +2,7 @@
 #
 # Screenshot: https://cloud.githubusercontent.com/assets/627486/23837059/47565150-073f-11e7-9a92-00fce65e72fc.png
 
-PATH="/usr/local/bin:$PATH"
+PATH="/opt/homebrew/bin:$PATH"
 
 gdate --utc +%dT%H:%MZ
 echo ---
