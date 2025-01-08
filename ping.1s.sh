b9515d0c64cd8eb7a3216c6a8258fd9aff3e77f9
Dan Brown <jdanbrown@gmail.com>
2025-01-08 16:15:30 -0700

    ping.1s.sh

--- ping.1s.sh
+++ ping.1s.sh
@@ -3,22 +3,27 @@
 # Inspired by https://github.com/matryer/bitbar-plugins/blob/master/Network/ping.10s.sh
 # Screenshot: https://cloud.githubusercontent.com/assets/627486/23837056/376cdf7a-073f-11e7-950f-a8ef26fbdae9.png
 
-PATH="/usr/local/bin:$PATH"
+# Docs:
+# - https://xbarapp.com/docs/
+# - https://github.com/matryer/xbar-plugins/blob/main/CONTRIBUTING.md
+
+PATH="/opt/homebrew/bin:$PATH"
 
 #home_wifi_gateway='192.168.0.1'
 #od_wifi_gateway='10.116.0.1'
-current_gateway="`route -n get default | sed -nE '/gateway:/ s/.*: (.*)/\1/ p'`"
+current_gateway="`route -n get default | gsed -nE '/gateway:/ s/.*: (.*)/\1/ p'`"
 
 icon_hosts=(
-  # '🔒 192.168.255.1'
-  '🌍 8.8.8.8'
-  "📶 $current_gateway"
+  "📶 $current_gateway" # wifi
+  # '🔒 192.168.255.1' # vpn
+  '🌍 1.1.1.1' # internet
+  '🌍 8.8.4.4' # internet
 )
 
 timeout_s=1
-history_lines=59
-line_format='color=gray size=10 font=Consolas trim=false'
-log_file='/tmp/bitbar-ping.log'
+history_lines=50
+line_format='disabled=true size=12 font=Consolas trim=false'
+log_file='/tmp/xbar-ping.log'
 
 timeouts=''
 printf '%-11s' "[`date +%H:%M:%S`]" >>"$log_file"
